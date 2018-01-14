65bf1436ecd7842a65ca5dead7eb657e7d94f16b
Aidan Jessen <aidan.jessen@gmail.com>
2018-01-14 12:45:49 +1000

    first working curl

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
 
-	string url = "";
+	string url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&key=AIzaSyD4TY0ecKKJWFvr_mVy96t5Pe2-yDaA7Gg";
 
 	curlTest( url );
 
--- maps.cpp
+++ maps.cpp
@@ -1,5 +1,32 @@
 #include "maps.h"
 
-void curlTest ( string urls ) {
+string data;
+
+size_t writeCallback ( char* buf, size_t size, size_t nmemb, void* up ) {
+
+	for ( int c = 0; c < size*nmemb; c++ ) {
+		data.push_back( buf[c] );
+	}
+
+	return size*nmemb;
+
+}
+
+void curlTest ( string url_string ) {
+
+	CURL* curl_handle;
+
+	curl_global_init( CURL_GLOBAL_ALL );
+	curl_handle = curl_easy_init();
+
+	curl_easy_setopt( curl_handle, CURLOPT_URL, url_string.c_str() );
+	curl_easy_setopt( curl_handle, CURLOPT_WRITEFUNCTION, &writeCallback );
+
+	curl_easy_perform( curl_handle );
+
+	cout << endl << data << endl;
+
+	curl_easy_cleanup( curl_handle );
+	curl_global_cleanup();
 
 }
